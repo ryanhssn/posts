175f77366d0751509985049fb82e9277c428cd40
Bilal H <hussain.bilal000786@gmail.com>
2018-01-29 23:51:47 +0530

    Add navigation & reduxForm

---
+++ src/components/posts_new.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        PostsNew!
+      </div>
+    )
+  }
+}
+
+
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
